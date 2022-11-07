1ca3c237b5e0c4ac92e9cce971c3df0db2f4bdde
Raden Fajrus <radenfajrus@gmail.com>
2022-11-07 17:24:51 +0700

    add api face detection

---
+++ app/face_detection/__init__.py
@@ -0,0 +1,104 @@
+
+
+# from keras.models import load_model
+
+import cv2
+import torch
+import numpy as np
+import os 
+
+# model = load_model('public/files/facenet_keras.h5')
+# model._make_predict_function()    
+
+from PIL import Image, ImageDraw
+
+from facenet_pytorch import MTCNN, InceptionResnetV1
+mtcnn = MTCNN()
+resnet = InceptionResnetV1(pretrained='vggface2').eval()
+
+from io import BytesIO
+import base64
+
+def detect(img_base64):   
+    frame = Image.open(BytesIO(base64.b64decode(img_base64)))
+    results = []
+    try:
+        # results = model.predict(img,batch_size=1,verbose = 2)[0]
+        boxes, probs, landmarks = mtcnn.detect(frame, landmarks=True)
+        results = boxes
+    except Exception as e:
+        print(e)
+
+    return results
+
+import glob
+
+def crop(id, img_base64):   
+    frame = Image.open(BytesIO(base64.b64decode(img_base64)))
+    results = []
+    buffered = BytesIO()
+
+    try:
+        # results = model.predict(img,batch_size=1,verbose = 2)[0]
+        boxes, probs, landmarks = mtcnn.detect(frame, landmarks=True)
+
+        if not os.path.isdir("public/assets/photo{}".format(id)):
+            os.makedirs("public/assets/photo/{}".format(id))
+
+        files = glob.glob('public/assets/photo/{}/*'.format(id))
+        for f in files:
+            os.remove(f)
+
+        frame.save("public/assets/photo/{}/base.jpg".format(id))
+
+        for idx,box in enumerate(boxes):
+            frame_draw = frame.copy()
+            img_cropped = frame_draw.crop(box)
+
+            img_cropped.save(buffered, format="JPEG")
+            img = base64.b64encode(buffered.getvalue())
+            img_cropped.save("public/assets/photo/{}/{}.jpg".format(id,idx))
+            results.append("/assets/photo/{}/{}.jpg".format(id,idx))
+
+    except Exception as e:
+        print(e)
+
+    return results
+
+
+if __name__ == "__main__":
+    print("img")
+    image = Image.open("public/college.jpg")
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    img = base64.b64encode(buffered.getvalue())
+
+    res = crop(img)
+    print(len(res))
+    # img_cropped = mtcnn(img, save_path="public/collegess.jpg")
+
+    # print("img_embedding")
+    # # Calculate embedding (unsqueeze to add batch dimension)
+    # img_embedding = resnet(img_cropped.unsqueeze(0))
+
+    # # Or, if using for VGGFace2 classification
+    # print("img_probs")
+    # resnet.classify = True
+    # img_probs = resnet(img_cropped.unsqueeze(0))
+
+    
+    # print("detect")
+    # frame = img
+    # # Detect faces
+    # boxes, _ = mtcnn.detect(frame)
+    # print(boxes)
+    
+    # print("draw")
+    # # Draw faces
+    # frame_draw = frame.copy()
+    
+    # print("tracked")
+    # # Add to frame list
+    # frames_tracked = frame_draw.resize((640, 360), Image.BILINEAR)
+    # frames_tracked.save("public/college-cropped.jpg")
+    # print(frames_tracked)
--- routers/default.py
+++ routers/default.py
@@ -1,19 +1,40 @@
-from fastapi import APIRouter
+import base64
+from fastapi import APIRouter, File, Form
 router = APIRouter()
 # from app.face_recognition import preprocess 
-# from app.face_recognition import detect 
+from app import face_recognition 
 
-# @router.get("/")
-# async def home():
-#     img = req.body
 
-#     img_clean = preprocess(img)
-#     results = detect(img_clean)
+from app import face_detection
+import uuid
 
-#     """ Test Home """
-#     return {"data": results}
-
-@router.get("/aaa")
+@router.get("/")
 async def home():
     """ Test Home """
-    return {"msg": "homeaaa"}
+    return {"msg": "home"}
+
+
+@router.post("/detect")
+async def detect_images(img: str = Form(...),date: str = Form(...)):
+    """ Upload File """
+    images = img
+    print(date)
+
+    id = date.replace(":","-").replace(".","-")
+    results = {
+        "base_img" : "assets/photo/{}/base.jpg".format(id),
+        "date" : date,
+        "status" : "complete",
+        "data": []
+    }
+    list_images = face_detection.crop(id,images)
+    for img in list_images:
+        result = {
+            "is_detected" : True,
+            "name" : str(uuid.uuid4()),
+            "nim" : str(uuid.uuid1())[0:8],
+            "img" : img,
+        }
+        results["data"].append(result)
+
+    return results
